b066302202a9b3a1bf3ebe100e739c11f9a59fb1
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-15 21:27:18 +0000

    added a function that checks if the input is ctrl+c  then exit. call the function from the setInout function

--- play.js
+++ play.js
@@ -4,3 +4,21 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function(callback) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (input) => {
+    callback(input);
+  });
+  return stdin;
+};
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+setupInput(handleUserInput);
+
