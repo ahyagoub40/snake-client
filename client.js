d8c5ba2702e8079c3c7b567cca450a3f0ccff0d8
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-15 20:37:42 +0000

    print name as soon as connection is established

--- client.js
+++ client.js
@@ -1,13 +1,17 @@
 const net = require('net');
 const connect = function() {
   const conn = net.createConnection({
-    host: '192.168.168.104',
+    host: '10.0.2.15',
     port: 50541
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+  
+  });
+  conn.on('connect', () => {
+    conn.write('Name: AYA');
   });
   return conn;
 };
