5a0733db61f5e15eecf6e1a2f3b9615182321030
ahyagoub40 <ahyagoub40@gmail.com>
2020-01-15 23:54:25 +0000

    created the connection variable through which we instructed the snake to move

--- input.js
+++ input.js
@@ -1,18 +1,39 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   stdin.on('data', (input) => {
+    console.log(input);
     handleUserInput(input);
   });
   return stdin;
 };
+
 const handleUserInput = function(key) {
+  console.log(connection);
+
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+  
+
 };
 
-setupInput();
+
 module.exports = setupInput;
